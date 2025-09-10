b7ab92cd20f90f5a75659c17b6c5c571fea89012
mbuck29 <micheal.buckendahl@icloud.com>
2025-09-09 21:22:56 -0500

    Updated the main loop
    Changes made:
    Initalized the board instance
    Checked for the pygame_gui events
    rendered the welcome screen

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import pygame
 import pygame_gui
 import config
 import sys
+from minesweeper.ui.view import draw_welcome
+from minesweeper.board import BoardGame
 
 
 pygame.init()
@@ -9,14 +11,27 @@ pygame.init()
 screen=pygame.display.set_mode((config.WINDOW_WIDTH ,config.WINDOW_HEIGHT))
 pygame.display.set_caption("Minesweeper")
 clock=pygame.time.Clock()
+manager = pygame_gui.UIManager((config.WINDOW_WIDTH, config.WINDOW_HEIGHT))
+board = BoardGame()
+text_box, start_button = draw_welcome(manager, screen)
+
 
 running=True
 while running:
-
+    dt = clock.tick(config.FPS) / 1000.0
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running=False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
+
+        manager.process_events(event)
+
+        if event.type == pygame_gui.UI_BUTTON_PRESSED and event.ui_element == start_button:
+            print("Start clicked. Mines:", text_box.get_text())
+            board.phase = 'playing'
+            text_box.hide()
+            start_button.hide()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 L_click_pos=event.pos
                 print(L_click_pos)
@@ -24,8 +39,12 @@ while running:
                 R_click_pos=event.pos
                 print(R_click_pos)
 
+    manager.update(dt)
+    if (board.phase == 'ready'):
+        draw_welcome(manager, screen, True)
 
-    screen.fill(config.COLOR_BACKGROUND)
+    if (board.phase == 'playing'):
+        screen.fill((0, 0, 0)) # This will be the call to draw_board
 
     pygame.display.flip()
 
--- minesweeper/ui/view.py
+++ minesweeper/ui/view.py
@@ -6,33 +6,36 @@ import pygame
 import pygame_gui
 from config import WINDOW_WIDTH, WINDOW_HEIGHT, FONT_NAME, FONT_SIZE, HELP_TEXT
 
-def draw_welcome(manager, screen):
-    screen.fill((0, 0, 0))
+def draw_welcome(manager, screen, text_only=False):
+    if text_only:
+        screen.fill((0, 0, 0))
 
-    # title
-    title_font = pygame.font.SysFont(FONT_NAME, FONT_SIZE + 12)
-    title_surface = title_font.render("Welcome to Minesweeper", True, (255, 255, 0))
-    screen.blit(title_surface, ((WINDOW_WIDTH - title_surface.get_width()) // 2, 60))
+        # title
+        title_font = pygame.font.SysFont(FONT_NAME, FONT_SIZE + 12)
+        title_surface = title_font.render("Welcome to Minesweeper", True, (255, 255, 0))
+        screen.blit(title_surface, ((WINDOW_WIDTH - title_surface.get_width()) // 2, 60))
 
-    #help text
-    help_font = pygame.font.SysFont(FONT_NAME, FONT_SIZE)
-    lines = HELP_TEXT.split('\n')
-    for i, line in enumerate(lines):
-        text_surface = help_font.render(line, True, (200, 200, 200))
-        screen.blit(text_surface, (50, 140 + i * 30))
+        #help text
+        help_font = pygame.font.SysFont(FONT_NAME, FONT_SIZE)
+        lines = HELP_TEXT.split('\n')
+        for i, line in enumerate(lines):
+            text_surface = help_font.render(line, True, (200, 200, 200))
+            screen.blit(text_surface, (50, 140 + i * 30))
 
-    #bomb input box 
-    textbox = pygame_gui.elements.UITextEntryLine(
-        relative_rect=pygame.Rect((WINDOW_WIDTH // 2 - 100, 350), (200, 30)),
-        manager=manager
-    )
+        manager.draw_ui(screen)
 
-    # Start game button
-    start_button = pygame_gui.elements.UIButton(
-        relative_rect=pygame.Rect((WINDOW_WIDTH // 2 - 50, 400), (100, 40)),
-        text='Start Game',
-        manager=manager
-    )
+    else:
+        #bomb input box 
+        textbox = pygame_gui.elements.UITextEntryLine(
+            relative_rect=pygame.Rect((WINDOW_WIDTH // 2 - 100, 350), (200, 30)),
+            manager=manager
+        )
 
- 
-    return textbox, start_button
+        # Start game button
+        start_button = pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect((WINDOW_WIDTH // 2 - 50, 400), (100, 40)),
+            text='Start Game',
+            manager=manager
+        )
+
+        return textbox, start_button
